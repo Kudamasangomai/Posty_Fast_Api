92dd44d1a7f16be1f1630bd35eab7375ffd8b3d2
Kudamasangomai <Kudam775@gmail.com>
2024-07-11 13:19:07 +0200

    empty field validation

--- schemas.py
+++ schemas.py
@@ -1,13 +1,13 @@
-from pydantic import (BaseModel,field_validator )
+from pydantic import (BaseModel,field_validator,Field )
 
 #used for validation 
 
 class Post(BaseModel):
-    post:str
+    post:str = Field(min_length = 3)
 
     @field_validator('post')
     @classmethod
     def not_empty(cls ,v):
         if not v.strip():
-            raise ValueError('must not be empty')
+            raise ValueError('post field must not be empty')
         return v
